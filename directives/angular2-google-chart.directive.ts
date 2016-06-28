ffb717a4a0dcc9ceb74e12342085379e2131d4cc
Martino Lessio <mlessio@users.noreply.github.com>
2016-06-28 10:01:39 +0200

    Update angular2-google-chart.directive.ts
    Fix package imports for angular2 rc supporting webpack build system

--- directives/angular2-google-chart.directive.ts
+++ directives/angular2-google-chart.directive.ts
@@ -1,4 +1,4 @@
-import {Directive,ElementRef,Input,OnInit} from 'angular2/core';
+import {Directive,ElementRef,Input,OnInit} from '@angular/core';
 declare var google:any;
 declare var googleLoaded:any;
 @Directive({
