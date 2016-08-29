5f023d0734cf1d72e4b4b8a6c3ec48a23ad2d10b
Róbert Kiss <ert78gb@gmail.com>
2016-08-29 17:15:51 +0200

    fix property definitions

--- src/directives/angular2-google-chart.directive.ts
+++ src/directives/angular2-google-chart.directive.ts
@@ -9,35 +9,36 @@ export class GoogleChartDirective implements OnInit {
   private _chartType: string;
   private _chartOptions: Object;
   private _chartData: Object;
+  private _inited: boolean;
 
   @Input('chartType')
-  public set chartType(value:string){
+  public set chartType(value: string) {
     this._chartType = value;
     this.draw();
   }
 
-  public get chartType():string{
+  public get chartType(): string {
     return this._chartType
   }
 
   @Input('chartOptions')
-  public set chartOptions(value:Object){
+  public set chartOptions(value: Object) {
     this._chartOptions = value;
     this.draw();
   }
 
-  public get chartOptions():Object{
-    return this.chartOptions
+  public get chartOptions(): Object {
+    return this._chartOptions
   }
 
   @Input('chartData')
-  public set chartData(value:Object){
+  public set chartData(value: Object) {
     this._chartData = value;
     this.draw();
   }
 
-  public get chartData():Object{
-    return this.chartData
+  public get chartData(): Object {
+    return this._chartData
   }
 
   constructor(public element: ElementRef) {
@@ -49,18 +50,20 @@ export class GoogleChartDirective implements OnInit {
       googleLoaded = true;
       google.charts.load('current', {'packages': ['corechart', 'gauge']});
     }
+    this._inited = true;
     this.draw();
   }
 
-  private draw():void{
+  private draw(): void {
+    if (!this._inited)
+      return;
     this.drawGraph(this.chartOptions, this.chartType, this.chartData, this._element);
   }
 
-  private drawGraph(chartOptions:Object, chartType:string, chartData:Object, ele:any) {
-    console.log('drawGraph');
+  private drawGraph(chartOptions: Object, chartType: string, chartData: Object, ele: any) {
     google.charts.setOnLoadCallback(drawChart);
     function drawChart() {
-      var wrapper:any;
+      var wrapper: any;
       wrapper = new google.visualization.ChartWrapper({
         chartType: chartType,
         dataTable: chartData,
