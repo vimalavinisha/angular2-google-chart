3536be35189b0ea9b98633a3e2abb29796727865
Stefan Vitz <stefan@vitz.ch>
2016-10-09 02:29:29 +0200

    implementing ngOnChange to become sensitive to changeing data

--- directives/angular2-google-chart.directive.ts
+++ directives/angular2-google-chart.directive.ts
@@ -1,41 +1,46 @@
-import {Directive,ElementRef,Input,OnInit} from '@angular/core';
+import {Directive, ElementRef, Input, OnInit, OnChanges} from '@angular/core';
 declare var google:any;
 declare var googleLoaded:any;
 @Directive({
-  selector: '[GoogleChart]',
-  // properties: [
-  //     'chartType',
-  //     'chartOptions',
-  //     'chartData'
-  //   ]
+    selector: '[GoogleChart]',
+    // properties: [
+    //     'chartType',
+    //     'chartOptions',
+    //     'chartData'
+    //   ]
 })
-export class GoogleChart implements OnInit {
-  public _element:any;
-  @Input('chartType') public chartType:string;
-  @Input('chartOptions') public chartOptions: Object;
-  @Input('chartData') public chartData: Object;
-  constructor(public element: ElementRef) {
-    this._element = this.element.nativeElement;
-  }
-  ngOnInit() {
-    if(!googleLoaded) {
-      googleLoaded = true;
-    google.charts.load('current', {'packages':['corechart', 'gauge']});
-     }
-    setTimeout(() =>this.drawGraph(this.chartOptions,this.chartType,this.chartData,this._element),1000);
-  }
-  drawGraph (chartOptions,chartType,chartData,ele) {
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var wrapper;
-       wrapper = new google.visualization.ChartWrapper({
-             chartType: chartType,
-             dataTable:chartData ,
-             options:chartOptions || {},
-             containerId: ele.id
-           });
-      wrapper.draw();
+export class GoogleChart implements OnInit, OnChanges {
+    public _element:any;
+    @Input('chartType') public chartType:string;
+    @Input('chartOptions') public chartOptions: Object;
+    @Input('chartData') public chartData: Object;
+    constructor(public element: ElementRef) {
+        this._element = this.element.nativeElement;
     }
-  }
+
+    ngOnInit() {
+        if(!googleLoaded) {
+            googleLoaded = true;
+            google.charts.load('current', {'packages':['corechart', 'gauge']});
+        }
+        setTimeout(() => this.drawGraph(this.chartOptions,this.chartType,this.chartData,this._element),1000);
+    }
+
+    ngOnChanges(changes) {
+        this.drawGraph(this.chartOptions,this.chartType,this.chartData,this._element);
+    }
+
+    drawGraph (chartOptions,chartType,chartData,ele) {
+        google.charts.setOnLoadCallback(drawChart);
+        function drawChart() {
+            var wrapper = new google.visualization.ChartWrapper({
+                chartType: chartType,
+                dataTable:chartData ,
+                options:chartOptions || {},
+                containerId: ele.id
+            });
+            wrapper.draw();
+        }
+    }
 }
 
